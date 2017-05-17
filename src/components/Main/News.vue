9cfc219fa279fcc8c8383a5bf917fee494196c86
GwonHyeok <me@ghyeok.io>
2017-05-17 18:32:10 +0900

    뉴스 레이아웃 수정

--- src/components/Main/News.vue
+++ src/components/Main/News.vue
@@ -1,26 +1,30 @@
 <template>
-  <section class="hero is-success is-fullheight">
+  <section class="hero  is-fullheight has-text-centered">
+
     <div class="hero-body">
-      <div class="left" v-on:mouseover="scrollToRight" v-on:mouseleave="scrollStop">
-        <i class="material-icons">keyboard_arrow_right</i>
+
+      <div class="left" v-on:mouseover="onMouseOver($event, 'left')" v-on:mouseout="this.onMouseOut">
+        <i class="material-icons">keyboard_arrow_left</i>
       </div>
+
       <div class="container">
-        <h1 class="title">
-          Full Height title
-        </h1>
-        <h2 class="subtitle">
-          Full Height subtitle
-        </h2>
+        <div class="title-container">
+          <h1 class="title is-1">안녕, 너는 <strong>데이코어의</strong> 기사를 보고있어.</h1>
+          <h2 class="subtitle is-6">여기에는 즐거운 데이코어 기사들이 있지 한번 확인해봐</h2>
+        </div>
         <div class="news-wrap" id="news">
           <div class="columns is-mobile">
-            <news-card class="column is-3-desktop is-4-tablet is-8-mobile" v-for="item in items"
+            <news-card class="news-card column is-3-desktop is-4-tablet is-8-mobile" v-for="item in items"
+                       v-bind:key="item.title"
                        v-bind:item="item"></news-card>
           </div>
         </div>
       </div>
-      <div class="right" v-on:mouseover="scrollToLeft" v-on:mouseleave="scrollStop">
-        <i class="material-icons">keyboard_arrow_left</i>
+
+      <div class="right" v-on:mouseover="onMouseOver($event, 'right')" v-on:mouseout="this.onMouseOut">
+        <i class="material-icons">keyboard_arrow_right</i>
       </div>
+
     </div>
   </section>
 </template>
@@ -29,6 +33,28 @@
   export default {
     components: {NewsCard},
     name: 'news',
+    watch: {
+      isScrolling: function (newScrollingStatus) {
+        // 새로운 스크롤 상태가 스크롤 중이라면 스크롤 서비스 실행
+        if (newScrollingStatus) {
+          const newsElement = document.querySelector('#news > .columns')
+          const maxWidth = document.querySelector('#news > .columns > .card').clientWidth * this.items.length
+
+          // 스크롤 핸들러 실행
+          this.scrollingHanddler = setInterval(() => {
+            let calcLeft = this.scrollingDirection === 'left' ? --this.left : ++this.left
+            if (calcLeft > 0) {
+              this.left = 0
+            } else if (calcLeft < -maxWidth) {
+              this.left = -maxWidth
+            }
+            newsElement.style.transform = `translate(${this.left}px, 0px)`
+          }, 0)
+        } else if (this.scrollingHanddler !== null) {
+          clearInterval(this.scrollingHanddler)
+        }
+      }
+    },
     data: function () {
       return {
         items: [
@@ -39,7 +65,7 @@
           },
           {
             title: '카파, 전문 스포츠 브랜드로 영역 확대',
-            description: '',
+            description: '확대를 하고 하고 또 하고',
             image: require('../../assets/news2.png')
           },
           {
@@ -74,50 +100,85 @@
           }
         ],
         left: 0,
-        scroll: false
+        scrollingDirection: 'right',
+        isScrolling: false
       }
     },
     methods: {
-      scrollToRight: function () {
-        console.log('scroll to right')
-        document.getElementById('news').style.marginLeft = --this.left + 'px'
+      onMouseOver: function (event, direction) {
+        if (this.isScrolling) return
+        this.scrollingDirection = direction
+        this.isScrolling = true
       },
-      scrollToLeft: function () {
-        console.log('scroll to left')
-        document.getElementById('news').style.marginLeft = ++this.left + 'px'
-      },
-      scrollStop: function () {
+      onMouseOut: function (event) {
+        this.isScrolling = false
       }
     }
   }
 </script>
-<style scoped="">
+<style lang="scss" scoped>
+
+  @mixin transition($animation...) {
+    -moz-transition: $animation;
+    -webkit-transition: $animation;
+    transition: $animation;
+  }
+
   .hero-body {
+    overflow: hidden;
     position: relative;
     padding: 0;
   }
 
   .container {
-    overflow: hidden;
     width: 100%;
-    max-width: 2000px;
+    overflow: hidden;
     margin: 0 auto;
+    max-width: inherit;
+  }
+
+  #news > .columns {
+    overflow: visible;
   }
 
   .news-wrap {
     width: 100%;
+    padding: 2.5rem;
+    background: rgb(240, 244, 244);
+  }
+
+  .news-card {
+    margin-left: 0.6rem;
+    margin-right: 0.6rem;
+
+    @include transition(opacity 0.75s ease-in-out)
+  }
+
+  .title-container {
+    padding: 1.5rem;
   }
 
   .left, .right {
     position: absolute;
-    top: calc(50% - 75px);
-    width: 150px;
-    height: 150px;
+    top: calc(50%);
+    width: 120px;
+    height: 110px;
     background-color: rgba(72, 57, 73, 0.5);
     z-index: 9;
     cursor: pointer;
     text-align: center;
-    line-height: 150px;
+    line-height: 110px;
+    color: white;
+
+    @include transition(background-color 0.35s ease-in-out)
+  }
+
+  .left:hover {
+    background-color: rgba(239, 131, 118, 0.75);
+  }
+
+  .right:hover {
+    background-color: rgba(239, 131, 118, 0.75);
   }
 
   .left {
--- src/components/Main/NewsCard.vue
+++ src/components/Main/NewsCard.vue
@@ -7,18 +7,9 @@
     </div>
     <div class="card-content">
       <div class="media">
-        <!--<div class="media-left">-->
-          <!--<figure class="image is-48x48">-->
-            <!--<img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">-->
-          <!--</figure>-->
-        <!--</div>-->
         <div class="media-content">
-          <p class="news-title is-4">{{item.title}}</p>
+          <p class="news-title title is-4">{{item.title}}</p>
         </div>
-      </div>
-      <div class="content">
-        <!--<p v-for="description in item.description.split('\\n')"></p>-->
-        {{item.description}}
       </div>
     </div>
   </div>
@@ -34,9 +25,6 @@
     font-size: 15pt;
     font-weight: 600;
     color: #333333;
-    word-break: keep-all;
-  }
-  .content {
     word-break: keep-all;
   }
 </style>
