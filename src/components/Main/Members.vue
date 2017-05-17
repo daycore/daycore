51371b1bb7317a340752abc2d1f555a2be06db09
GwonHyeok <me@ghyeok.io>
2017-05-17 18:53:34 +0900

    멤버 페이지 추가

---
+++ src/components/Main/Members.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">데이코어 식구들</h1>
+        <h1 class="subtitle">식구가 뭐야 먹는거야?</h1>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'members'
+  }
+</script>
+<style scoped>
+  .hero.is-fullheight {
+    background: url(../../assets/bg_members.png) no-repeat fixed center center;
+    background-size: cover;
+  }
+</style>
