83a353b1abdb3b92aba052fcb964ce3c509fe1c8
GwonHyeok <me@ghyeok.io>
2017-05-18 15:00:36 +0900

    멤버 데이터 업데이트

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,7 +4,7 @@
     <my-header></my-header>
     <about></about>
     <news></news>
-    <members></members>
+    <members :members="members"></members>
 
   </div>
 </template>
@@ -25,7 +25,19 @@
     name: 'hello',
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        msg: 'Welcome to Your Vue.js App',
+        members: [
+          {name: '최경민', position: '대표', image: 'http://daycore.co.kr/img/section07_team1_hover.png'},
+          {name: '권혁', position: '개발'},
+          {name: '김동환', position: '매니저'},
+          {name: '구용석', position: '매니저'},
+          {name: '고현주', position: '개발'},
+          {name: '박관호', position: '개발'},
+          {name: '송진표', position: '개발'},
+          {name: '우동오', position: '개발'},
+          {name: '장우영', position: '디자이너'},
+          {name: '정현지', position: '개발'}
+        ]
       }
     }
   }
