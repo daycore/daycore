a1a55382b1e639c409b0a1c67c7aaccd113e680f
GwonHyeok <me@ghyeok.io>
2017-05-18 17:15:27 +0900

    파일 경로 수정

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -29,57 +29,58 @@
       return {
         msg: 'Welcome to Your Vue.js App',
         members: [
-          {name: '최경민', position: '대표', image: 'http://daycore.co.kr/img/section07_team1_hover.png'},
-          {name: '권혁', position: '개발'},
-          {name: '김동환', position: '매니저'},
-          {name: '구용석', position: '매니저'},
-          {name: '고현주', position: '개발'},
-          {name: '박관호', position: '개발'},
-          {name: '송진표', position: '개발'},
-          {name: '우동오', position: '개발'},
-          {name: '장우영', position: '디자이너'},
-          {name: '정현지', position: '개발'}
+          {name: '최경민', position: '대표', image: 'http://daycore.co.kr/img/section07_team1.png'},
+          {name: '권혁', position: '개발', image: 'http://daycore.co.kr/img/section07_team2.png'},
+          {name: '김동환', position: '매니저', image: 'http://daycore.co.kr/img/section07_team2.png'},
+          {name: '구용석', position: '매니저', image: 'http://daycore.co.kr/img/section07_team2.png'},
+          {name: '고현주', position: '개발', image: 'http://daycore.co.kr/img/section07_team2.png'},
+          {name: '박관호', position: '개발', image: 'http://daycore.co.kr/img/section07_team2.png'},
+          {name: '송진표', position: '개발', image: 'http://daycore.co.kr/img/section07_team2.png'},
+          {name: '이준', position: '랩짐데이', image: 'http://daycore.co.kr/img/section07_team2.png'},
+          {name: '우동오', position: '개발', image: 'http://daycore.co.kr/img/section07_team2.png'},
+          {name: '장우영', position: '디자이너', image: 'http://daycore.co.kr/img/section07_team2.png'},
+          {name: '정현지', position: '개발', image: 'http://daycore.co.kr/img/section07_team2.png'}
         ],
         articles: [
           {
             title: 'G.PACE 출시',
             description: '운동코치 짐데이, 블루투스 연동 스마트 체중계 G.PACE 출시',
-            image: require('@/assets/news1.png')
+            image: require('@/assets/articles/news1.png')
           },
           {
             title: '카파, 전문 스포츠 브랜드로 영역 확대',
             description: '확대를 하고 하고 또 하고',
-            image: require('@/assets/news2.png')
+            image: require('@/assets/articles/news2.png')
           },
           {
             title: '운동코치 짐데이, 피트니스 페스티벌 진행',
             description: '모바일 운동코치 짐데이, 20일 피트니스 페스티벌 진행',
-            image: require('@/assets/news3.png')
+            image: require('@/assets/articles/news3.png')
           },
           {
             title: '원앤파트너스 등으로부터 투자유치',
             description: '"운동코치 짐데이" 데이코어, 원앤파트너스 등으로부터 투자유치',
-            image: require('@/assets/news4.png')
+            image: require('@/assets/articles/news4.png')
           },
           {
             title: '스포츠 산업 "잡콘서트" 개최',
             description: '스포츠 산업 "잡콘서트" 개최...\n취업.창업, 트렌드에 민감하라',
-            image: require('@/assets/news5.png')
+            image: require('@/assets/articles/news5.png')
           },
           {
             title: '비싼 PT, 앱 하나면 끝',
             description: '삼성.네이버 출신 "몸짱" 개발자 "비싼 PT, 앱 하나면 끝"',
-            image: require('@/assets/news6.png')
+            image: require('@/assets/articles/news6.png')
           },
           {
             title: '"짐데이"오늘의 앱으로 선정',
             description: '[오늘의 앱]스마트폰이 피트니스 코치로..."짐데이"',
-            image: require('@/assets/news7.png')
+            image: require('@/assets/articles/news7.png')
           },
           {
             title: '으뜸앱 선정',
             description: '대한민국 모바일 어워드 5월의 으뜸앱 선정',
-            image: require('@/assets/news8.png')
+            image: require('@/assets/articles/news8.png')
           }
         ]
       }
