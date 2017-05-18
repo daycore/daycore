9b732dfc51f91f1b847efb5c74c89fbf72cdc5eb
GwonHyeok <me@ghyeok.io>
2017-05-18 16:34:55 +0900

    뉴스 기사 항목 props 로 변경

--- src/components/Main/News.vue
+++ src/components/Main/News.vue
@@ -14,7 +14,7 @@
         </div>
         <div class="news-wrap" id="news">
           <div class="columns is-mobile">
-            <news-card class="news-card column is-3-desktop is-4-tablet is-8-mobile" v-for="item in items"
+            <news-card class="news-card column is-3-desktop is-4-tablet is-8-mobile" v-for="item in articles"
                        v-bind:key="item.title"
                        v-bind:item="item"></news-card>
           </div>
@@ -33,12 +33,13 @@
   export default {
     components: {NewsCard},
     name: 'news',
+    props: ['articles'],
     watch: {
       isScrolling: function (newScrollingStatus) {
         // 새로운 스크롤 상태가 스크롤 중이라면 스크롤 서비스 실행
         if (newScrollingStatus) {
           const newsElement = document.querySelector('#news > .columns')
-          const maxWidth = document.querySelector('#news > .columns > .card').clientWidth * this.items.length
+          const maxWidth = document.querySelector('#news > .columns > .card').clientWidth * this.articles.length
 
           // 스크롤 핸들러 실행
           this.scrollingHanddler = setInterval(() => {
@@ -57,48 +58,6 @@
     },
     data: function () {
       return {
-        items: [
-          {
-            title: 'G.PACE 출시',
-            description: '운동코치 짐데이, 블루투스 연동 스마트 체중계 G.PACE 출시',
-            image: require('../../assets/news1.png')
-          },
-          {
-            title: '카파, 전문 스포츠 브랜드로 영역 확대',
-            description: '확대를 하고 하고 또 하고',
-            image: require('../../assets/news2.png')
-          },
-          {
-            title: '운동코치 짐데이, 피트니스 페스티벌 진행',
-            description: '모바일 운동코치 짐데이, 20일 피트니스 페스티벌 진행',
-            image: require('../../assets/news3.png')
-          },
-          {
-            title: '원앤파트너스 등으로부터 투자유치',
-            description: '"운동코치 짐데이" 데이코어, 원앤파트너스 등으로부터 투자유치',
-            image: require('../../assets/news4.png')
-          },
-          {
-            title: '스포츠 산업 "잡콘서트" 개최',
-            description: '스포츠 산업 "잡콘서트" 개최...\n취업.창업, 트렌드에 민감하라',
-            image: require('../../assets/news5.png')
-          },
-          {
-            title: '비싼 PT, 앱 하나면 끝',
-            description: '삼성.네이버 출신 "몸짱" 개발자 "비싼 PT, 앱 하나면 끝"',
-            image: require('../../assets/news6.png')
-          },
-          {
-            title: '"짐데이"오늘의 앱으로 선정',
-            description: '[오늘의 앱]스마트폰이 피트니스 코치로..."짐데이"',
-            image: require('../../assets/news6.png')
-          },
-          {
-            title: '으뜸앱 선정',
-            description: '대한민국 모바일 어워드 5월의 으뜸앱 선정',
-            image: require('../../assets/news7.png')
-          }
-        ],
         left: 0,
         scrollingDirection: 'right',
         isScrolling: false
