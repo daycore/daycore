27adea9353ebb36251b3ceacaeb1f2d35e1904a7
GwonHyeok <me@ghyeok.io>
2017-05-18 17:57:43 +0900

    멤버 레이아웃 수정

--- src/components/Main/Members.vue
+++ src/components/Main/Members.vue
@@ -5,8 +5,8 @@
         <h1 class="title is-2">데이코어 식구들</h1>
         <h1 class="subtitle is-4">식구가 뭐야 먹는거야?</h1>
 
-        <div class="columns is-multiline">
-          <member-card v-for="member in members" v-bind:key="member.name" class="column is-3-desktop is-4-tablet is-6-mobile" :member="member"></member-card>
+        <div class="columns is-mobile is-multiline">
+          <member-card v-for="member in members" v-bind:key="member.name" class="column is-2-desktop is-3-tablet is-6-mobile" :member="member"></member-card>
         </div>
       </div>
     </div>
