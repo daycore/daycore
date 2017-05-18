5bf2acc09d2b11900ebfc8733d3bdca934a4eea6
GwonHyeok <me@ghyeok.io>
2017-05-18 14:59:59 +0900

    멤버, 멤버 카드 컴포넌트 추가

---
+++ src/components/Main/MemberCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4">{{member.name}}</p>
+          <p class="subtitle is-6">{{member.position}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'member-card',
+    props: ['member']
+  }
+</script>
+
+<style scoped>
+  .title {
+    margin-bottom: 1.8rem;
+  }
+</style>
