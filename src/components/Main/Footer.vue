3a7cdc28f8377ce5ef0155788288897caf588365
hyunju <hjko_o@daycore.co.kr>
2017-05-24 19:28:01 +0900

    정보 수정, 아이콘 추가

--- src/components/Main/Footer.vue
+++ src/components/Main/Footer.vue
@@ -6,12 +6,21 @@
         <p class="title is-5">
           <strong>데이코어 주식회사</strong>
         </p>
-        <p class="subtitle is-6">주소 : 서울특별시 강남구 역삼로 169, 명우빌딩 5층 | 대표전화 : 070-7501-5926</p>
-        <p class="subtitle is-6">사업자등록번호 : 144-81-14007 | 대표자 : 최경민 | 통신판매업신고번호 : 제2014-경기성남-0437호</p>
-
+        <p class="subtitle is-6">주소 : 서울특별시 강남구 도산대로 55길 45 2층 | 대표전화 : 070-7501-5926</p>
+        <p class="subtitle is-6">사업자등록번호 : 144-81-14007 | 대표자 : 최경민 | 통신판매업신고번호 : 제2016-서울강남-03264호</p>
+        <p class="subtitle is-6"><i class="fa fa-envelope"></i> info@daycore.co.kr</p>
         <p>
           <a class="icon" href="https://github.com/jgthms/bulma">
             <i class="fa fa-github"></i>
+          </a>
+          <a class="icon" href="https://www.facebook.com/gymdaykorea">
+            <i class="fa fa-facebook-official"></i>
+          </a>
+          <a class="icon" href="http://instagram.com/gymday_official">
+            <i class="fa fa-instagram"></i>
+          </a>
+          <a class="icon" href="http://youtube.com">
+            <i class="fa fa-youtube"></i>
           </a>
         </p>
 
