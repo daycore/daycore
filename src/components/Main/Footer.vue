14b1065223f7856b213a87ebd7843cc98577e34f
hyunju <hjko_o@daycore.co.kr>
2017-05-25 13:44:36 +0900

    소셜 아이콘 이미지 추가

--- src/components/Main/Footer.vue
+++ src/components/Main/Footer.vue
@@ -10,18 +10,19 @@
         <p class="subtitle is-6">사업자등록번호 : 144-81-14007 | 대표자 : 최경민 | 통신판매업신고번호 : 제2016-서울강남-03264호</p>
         <p class="subtitle is-6"><i class="fa fa-envelope"></i> info@daycore.co.kr</p>
         <p>
-          <a class="icon" href="https://github.com/jgthms/bulma">
-            <i class="fa fa-github"></i>
-          </a>
           <a class="icon" href="https://www.facebook.com/gymdaykorea">
-            <i class="fa fa-facebook-official"></i>
+            <img src="../../assets/footer_facebook_color.png">
           </a>
           <a class="icon" href="http://instagram.com/gymday_official">
-            <i class="fa fa-instagram"></i>
+            <img src="../../assets/footer_instagram_color.png">
           </a>
           <a class="icon" href="http://youtube.com">
-            <i class="fa fa-youtube"></i>
+            <img src="../../assets/footer_youtube_color.png">
           </a>
+          <a class="icon" href="">
+            <img src="../../assets/footer_naver_color.png">
+          </a>
+
         </p>
 
 
@@ -37,8 +38,6 @@
 </script>
 
 <style lang="scss" scoped>
-  .footer {
-  }
 
   p {
     color: black;
@@ -47,6 +46,12 @@
   .subtitle {
     margin-top: -30px;
     padding-top: 18px;
+  }
+
+  .icon {
+    width: 50px;
+    margin: 20px;
+    vertical-align: top;
   }
 
 </style>
