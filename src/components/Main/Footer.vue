30c0b73a9214e59707025992a5f8bf64a69b65f6
GwonHyeok <me@ghyeok.io>
2017-05-29 11:50:19 +0900

    푸터 이미지 업데이트

--- src/components/Main/Footer.vue
+++ src/components/Main/Footer.vue
@@ -9,18 +9,18 @@
         <p class="subtitle is-6">주소 : 서울특별시 강남구 도산대로 55길 45 2층 | 대표전화 : 070-7501-5926</p>
         <p class="subtitle is-6">사업자등록번호 : 144-81-14007 | 대표자 : 최경민 | 통신판매업신고번호 : 제2016-서울강남-03264호</p>
         <p class="subtitle is-6"><i class="fa fa-envelope"></i> info@daycore.co.kr</p>
-        <p>
-          <a class="icon" href="https://www.facebook.com/gymdaykorea">
-            <img src="../../assets/footer_facebook_color.png"/>
+        <p class="icon-group">
+          <a class="icon circle" href="https://www.facebook.com/gymdaykorea">
+            <img src="../../assets/footers/ic_facebook.png"/>
           </a>
-          <a class="icon" href="http://instagram.com/gymday_official">
-            <img src="../../assets/footer_instagram_color.png"/>
+          <a class="icon circle" href="http://instagram.com/gymday_official">
+            <img src="../../assets/footers/ic_instagram.png"/>
           </a>
-          <a class="icon" href="https://www.youtube.com/channel/UC0mtRNjXDzvpauAQSmX65MQ">
-            <img src="../../assets/footer_youtube_color.png"/>
+          <a class="icon circle" href="https://www.youtube.com/channel/UC0mtRNjXDzvpauAQSmX65MQ">
+            <img src="../../assets/footers/ic_youtube.png"/>
           </a>
-          <a class="icon" href="http://post.naver.com/my.nhn?memberNo=11714168">
-            <img src="../../assets/footer_naver_color.png"/>
+          <a class="icon circle" href="http://post.naver.com/my.nhn?memberNo=11714168">
+            <img src="../../assets/footers/ic_naver.png"/>
           </a>
         </p>
 
@@ -49,10 +49,25 @@
     padding-top: 18px;
   }
 
+  .icon-group {
+    margin-top: 2.2rem;
+  }
+
   .icon {
     width: 30px;
     margin: 0 10px;
     vertical-align: top;
   }
 
+  .icon.circle > img {
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    border-radius: 20%;
+  }
+
+  .fa-envelope {
+    font-size: 12px;
+    vertical-align: middle;
+    color: gray;
+  }
 </style>
