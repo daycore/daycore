044d8daa39a63ca13983b05f2509beecf53d0694
GwonHyeok <me@ghyeok.io>
2017-05-29 11:27:27 +0900

    contact-us 메일 전송 구현

--- src/components/Main/ContactUs.vue
+++ src/components/Main/ContactUs.vue
@@ -5,56 +5,21 @@
         <div class="contact">
           <h3 class="title is-spaced">Contant Us</h3>
           <div class="field">
-            <label class="label">Username</label>
-            <p class="control has-icons-left has-icons-right">
-              <input class="input" type="text" placeholder="보내는 사람">
-              <span class="icon is-small is-left">
-                <i class="fa fa-user"></i>
-              </span>
-              <!--<span class="icon is-small is-right">-->
-                <!--<i class="fa fa-check"></i>-->
-              <!--</span>-->
+            <label class="label">Subject</label>
+            <p class="control">
+              <input v-model="subject" class="input" type="text">
             </p>
-            <!--<p class="help is-success">This username is available</p>-->
           </div>
-          <div class="field">
-            <label class="label">Email</label>
-            <p class="control has-icons-left has-icons-right">
-              <input class="input" type="text" placeholder="이메일 주소">
-              <span class="icon is-small is-left">
-                <i class="fa fa-envelope"></i>
-              </span>
-              <!--<span class="icon is-small is-right">-->
-                <!--<i class="fa fa-warning"></i>-->
-              <!--</span>-->
-            </p>
-            <!--<p class="help is-danger">이메일 형식이 옳지 않습니다. 다시 입력해주세요:(</p>-->
-          </div>
-          <div class="field">
-            <label class="label">Phone Number</label>
-            <p class="control has-icons-left has-icons-right">
-              <input class="input" type="text" placeholder="연락처(ex.010-1234-5678)">
-              <span class="icon is-small is-left">
-                <i class="fa fa-phone"></i>
-              </span>
-              <!--<span class="icon is-small is-right">-->
-                <!--<i class="fa fa-warning"></i>-->
-              <!--</span>-->
-            </p>
-            <!--<p class="help is-danger">연락처 형식이 옳지 않습니다. 다시 입력해주세요:(</p>-->
-          </div>
+
           <div class="field">
             <label class="label">Message</label>
             <p class="control">
-              <textarea class="textarea" placeholder="이메일 내용을 입력해주세요"></textarea>
+              <textarea v-model="message" class="textarea"></textarea>
             </p>
           </div>
           <div class="columns field">
-            <div class="column is-6-desktop is-6-tablet is-12-phone">
-              <a class="button is-primary">Submit</a>
-            </div>
-            <div class="column is-6-desktop is-6-tablet is-12-phone">
-              <a class="button is-danger">Cancel</a>
+            <div class="column is-12-desktop is-12-tablet is-12-phone">
+              <a v-on:click="sendMail" class="button is-primary">Submit</a>
             </div>
           </div>
         </div>
@@ -66,7 +31,18 @@
 <script>
 
   export default {
-    name: 'contact-us'
+    name: 'contact-us',
+    data: function () {
+      return {
+        subject: '', message: ''
+      }
+    },
+    methods: {
+      sendMail () {
+        const query = encodeURI(`subject=${this.subject}&body=${this.message}`)
+        window.location.href = `mailto:info@daycore.co.kr?${query}`
+      }
+    }
   }
 </script>
 
